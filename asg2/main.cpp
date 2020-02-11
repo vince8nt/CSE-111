4686a673b2278861538356d433a13b34a54043e6
Vincent Titterton <57516281+vince8nt@users.noreply.github.com>
2020-02-10 19:02:36 -0800

    and user input parsing

--- asg2/main.cpp
+++ asg2/main.cpp
@@ -1,87 +1,140 @@
-// $Id: main.cpp,v 1.10 2019-10-08 13:55:31-07 - - $
+// $Id: main.cpp,v 1.11 2018-01-25 14:19:29-08 - - $
 
 #include <cstdlib>
+#include <exception>
 #include <iostream>
 #include <string>
-#include <utility>
 #include <unistd.h>
 
 using namespace std;
 
-#include "commands.h"
-#include "debug.h"
-#include "file_sys.h"
+#include "listmap.h"
+#include "xpair.h"
 #include "util.h"
 
-// scan_options
-//    Options analysis:  The only option is -Dflags. 
+using str_str_map = listmap<string,string>;
+using str_str_pair = str_str_map::value_type;
 
 void scan_options (int argc, char** argv) {
    opterr = 0;
    for (;;) {
       int option = getopt (argc, argv, "@:");
-      if (option == EOF) break;
+      if (option == EOF) break; // reached end of file
       switch (option) {
          case '@':
             debugflags::setflags (optarg);
             break;
          default:
-            complain() << "-" << static_cast<char> (option)
-                       << ": invalid option" << endl;
+            complain() << "-" << char (optopt) << ": invalid option"
+                       << endl;
             break;
       }
    }
-   if (optind < argc) {
-      complain() << "operands not permitted" << endl;
-   }
 }
 
-
-// main -
-//    Main program which loops reading commands until end of file.
+string delete_whitespace (string cur) {
+   // delete front and back spaces
+   // doesn't remove middle whitespace on purpose
+   while (cur.length() > 0 && (cur.at(0) == ' ' || cur.at(0) == '\t'))
+      cur = cur.substr(1);
+   while (cur.length() > 0 && (cur.at(cur.length() - 1) == ' '
+      || cur.at(cur.length() - 1) == '\t'))
+      cur = cur.substr(0, cur.length() - 1);
+   return cur;
+}
 
 int main (int argc, char** argv) {
-   exec::execname (argv[0]);
-   cout << boolalpha;  // Print false or true instead of 0 or 1.
-   cerr << boolalpha;
-   cout << argv[0] << " build " << __DATE__ << " " << __TIME__ << endl;
+   sys_info::execname (argv[0]);
    scan_options (argc, argv);
-   bool need_echo = want_echo();
-   inode_state state;
-   try {
-      for (;;) {
-         try {
-            // Read a line, break at EOF, and echo print the prompt
-            // if one is needed.
-            cout << state.prompt();
-            string line;
-            getline (cin, line);
-            if (cin.eof()) {
-               if (need_echo) cout << "^D";
-               cout << endl;
-               DEBUGF ('y', "EOF");
-               break;
-            }
-            if (need_echo) cout << line << endl;
-   
-            // Split the line into words and lookup the appropriate
-            // function.  Complain or call it.
-            wordvec words = split (line, " \t");
-            DEBUGF ('y', "words = " << words);
-            if ( words.at(0).at(0) != '#') {
-               command_fn fn = find_command_fn (words.at(0));
-               fn (state, words);
-            }
-         }catch (command_error& error) {
-            // If there is a problem discovered in any function, an
-            // exn is thrown and printed here.
-            complain() << error.what() << endl;
-         }
-      }
-   } catch (ysh_exit&) {
-      // This catch intentionally left blank.
+
+   str_str_map test;
+   for (char** argp = &argv[optind]; argp != &argv[argc]; ++argp) {
+      str_str_pair pair (*argp, to_string<int> (argp - argv));
+      cout << "Before insert: " << pair << endl;
+      test.insert (pair);
    }
 
-   return exit_status_message();
+   for (str_str_map::iterator itor = test.begin();
+        itor != test.end(); ++itor) {
+      cout << "During iteration: " << *itor << endl;
+   }
+
+   str_str_map::iterator itor = test.begin();
+   test.erase (itor);
+
+   for (;;) {
+      try {
+         cout << "-: ";
+         string line;
+         getline (cin, line);
+         line = delete_whitespace(line);
+         if (line.length() == 0) {
+            throw 0;
+         }
+         else if (line.at(0) == '#') {
+            // comment
+            // do nothing
+         }
+         else {
+            long equ = -1;
+            for (int index = 0;
+               index < static_cast<int>(line.length()); ++index) {
+               if (line.at(index) == '=') {
+                  if (equ != -1) throw 0;
+                  equ = index;
+               }
+            }
+            if (equ == -1) {
+               string key = line;
+               // no equals present
+               // given key, find value
+               cout << "given key: \"" << key
+                  << "\", must find the value.\n";
+            }
+            else if (line.length() == 1) { // and it is an '='
+               // print all the key and value pairs
+               cout << "must print all key value pairs\n";
+            }
+            else if (equ == 0) {
+               string value = line.substr(1);
+               value = delete_whitespace(value);
+               // equals at begining
+               // given value, find key
+               cout << "given value: \"" << value
+                  << "\", must find key.\n";
+            }
+            else if (equ == static_cast<int>(line.length()) - 1) {
+               string key = line.substr(0, line.length() - 1);
+               key = delete_whitespace(key);
+               // equals at end
+               // given key, delete key value pair
+               cout << "given key: \"" << key
+                  << "\", must delete the pair.\n";
+            }
+            else {
+               string key = line.substr(0, equ);
+               string value =
+                  line.substr(equ + 1, line.length() - equ - 1);
+               key = delete_whitespace(key);
+               value = delete_whitespace(value);
+               // equals in the middle
+               // create/modify key value pair
+               cout << "given key: \"" << key
+                  << "\", must set the value to \""
+                  << value << "\".\n";
+            }
+         }
+      }
+      catch (int exe) {
+         cout << "invalid input\n";
+      }
+   }
+   
+   
+
+   
+
+   cout << "EXIT_SUCCESS" << endl;
+   return EXIT_SUCCESS;
 }
 
